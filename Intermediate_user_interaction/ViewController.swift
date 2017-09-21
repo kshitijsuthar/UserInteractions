c2bee52bb4792838a50ac5b08da84d925ab9a3a8
Kshitij Suthar <kshitijsuthar@kshitijs-macbook-pro.local>
2017-09-20 21:58:45 -0400

    Code added

--- Intermediate_user_interaction/ViewController.swift
+++ Intermediate_user_interaction/ViewController.swift
@@ -4,19 +4,47 @@
  *Date:  September 20,2017
  *StudentID: 
  *Description: Intermediate User interactions demo
- *Version: 0.1 - Basic UI
+ *Version: 0.2 - Code Added
 
  */
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTextfield: UITextField!
 
+    @IBOutlet weak var numberTextfield: UITextField!
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet weak var leftSwitch: UISwitch!
+    
+    @IBOutlet weak var rightSwitch: UISwitch!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
 
+    @IBAction func sliderControl(_ sender: UISlider) {
+    }
+  
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func doSomethingButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSwitch(_ sender: UISwitch) {
+    
+    }
+    
+    
 }
+
 
